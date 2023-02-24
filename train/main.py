e9e082c6de36a5f263ea545242820fd0dca49172
Dan Fellin <dan@highwaterlabs.com>
2023-02-24 15:28:18 -0800

    getting there

--- train/main.py
+++ train/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import os
+import openai
 from prisma import Prisma
 
 
@@ -11,7 +13,20 @@ def main() -> None:
     for func in db.polyfunction.find_many():
         print(func)
 
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    response = openai.Completion.create(
+        model="code-davinci-002",
+        prompt='"""1. Create a list of first names\n2. Create a list of last names\n3. Combine them randomly into a list of 100 full names\n"""',
+        temperature=0,
+        max_tokens=300,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    print(response)
+
     db.disconnect()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
