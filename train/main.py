9d179e9193578a041ac1b6a60e618df808f0369a
Dan Fellin <dan@highwaterlabs.com>
2023-02-24 12:10:15 -0800

    seamless

--- train/main.py
+++ train/main.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+from prisma import Prisma
 
-if __name__ == "__main__":
-    print("hi world")
+
+def main() -> None:
+    db = Prisma()
+    db.connect()
+
+    # write your queries here
+    print("foo")
+    for func in db.polyfunction.find_many():
+        print(func)
+
+    db.disconnect()
+
+if __name__ == '__main__':
+    main()
