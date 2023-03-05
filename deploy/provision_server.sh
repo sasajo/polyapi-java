1abedc8c6e4450433bc948473c0f7e3ce6d22379
Dan Fellin <dan@highwaterlabs.com>
2023-03-05 09:56:50 -0800

    next

--- deploy/provision_server.sh
+++ deploy/provision_server.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 git pull
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt update
-sudo apt install -y python3-pip vim # redis
+sudo apt install -y nodejs python3-pip vim # redis
+npm install yarn
+# maybe do this instead:
+# curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
+yarn install
 # TODO add OPENAI_API_KEY env variable
 # also add other variables?
