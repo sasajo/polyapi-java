ffc3b47895eebc8d2a04a60c3a3e053c80762bd9
Dan Fellin <dan@highwaterlabs.com>
2023-03-07 07:21:18 -0800

    onward

--- science/train_new_model.py
+++ science/train_new_model.py
@@ -42,3 +42,39 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+
+# TODO replace transforming the CSV to jsonl and uploading that
+# with transforming the contents of the DB to jsonl and uploading that?
+# async function main() {
+#   async function getUntrained(): Promise<PolyFunction[]> {
+#     const rv = prisma.polyFunction.findMany();
+#     return rv;
+#   }
+
+#   async function markAsTrained(id: number) {
+#     await prisma.polyFunction.update({
+#       where: {
+#         id: id,
+#       },
+#       data: {
+#         trained: false,
+#       },
+#     });
+#   }
+
+#   async function train() {
+#     const funcs = await getUntrained();
+#     console.log(`Now training on ${funcs.length} functions...`);
+#     for (const func of funcs) {
+#       //openai.Completion.create(
+#       // prompt=prompt,
+#       // engine="text-davinci-..."
+#       // send to chatGPT
+#       // on success:
+#       await markAsTrained(func.id);
+#     }
+#   }
+
+#   train();
+# }
