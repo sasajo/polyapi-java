d94f34c8b19e9199e1f4b8e7a3add2d8407b7243
Dan Fellin <dan@highwaterlabs.com>
2023-03-09 15:24:50 -0800

    hacking

--- science/train_new_model.py
+++ science/train_new_model.py
@@ -29,8 +29,9 @@ def transform_to_jsonl() -> str:
     return str(jsonl_path)
 
 
-def main() -> None:
-    jsonl_path = transform_to_jsonl()
+def start_fine_tune() -> None:
+    abs_path = Path(__file__).parent
+    jsonl_path = (abs_path / "data/examples.jsonl").resolve()
     upload = openai.File.create(file=open(jsonl_path, "rb"), purpose="fine-tune")
     resp = openai.FineTune.create(training_file=upload.id, model="davinci")
     print("To follow the model and see when it's done, please run the following:")
@@ -38,7 +39,14 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    # STEP 1, run this:
+    transform_to_jsonl()
+
+    # STEP 2
+    # add completion data to the jsonl
+
+    # STEP 3
+    # start_fine_tune()
 
 
 # TODO replace transforming the CSV to jsonl and uploading that
