f1295675c674341f761b1d36b733565ca082d266
Dan Fellin <dan@highwaterlabs.com>
2023-03-10 12:33:46 -0800

    split into two scripts

---
+++ science/start_fine_tune.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import openai
+
+
+def start_fine_tune() -> None:
+    abs_path = Path(__file__).parent
+    jsonl_path = (abs_path / "data/examples.jsonl").resolve()
+    upload = openai.File.create(file=open(jsonl_path, "rb"), purpose="fine-tune")
+    resp = openai.FineTune.create(training_file=upload.id, model="davinci")
+    print("To follow the model and see when it's done, please run the following:")
+    print(f"openai api fine_tunes.follow -i {resp['id']}")
+
+    # TODO
+    # mark all funcs as trained (aka fine tuned)?
--- science/train_new_model.py
+++ science/train_new_model.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from pathlib import Path
 import json
-import openai
 from prisma import Prisma
-from science.utils import get_function_completion_question
+from science.utils import (
+    get_function_completion_answer,
+    get_function_completion_question,
+)
 from server import get_base_prompt
 
 db = Prisma()
@@ -18,9 +20,13 @@ def transform_to_jsonl() -> str:
     for func in db.polyfunction.find_many(where={"NOT": {"description": ""}}):  # type: ignore
         question = get_function_completion_question(f"how do I {func.description}?")
         parts = [base_prompt, question]
-        # TODO get completion
         prompt = "\n\n".join(parts)
-        data.append({"prompt": prompt, "completion": ""})
+        data.append(
+            {
+                "prompt": prompt,
+                "completion": get_function_completion_answer(base_prompt, question),
+            }
+        )
 
     abs_path = Path(__file__).parent
     jsonl_path = (abs_path / "data/examples.jsonl").resolve()
@@ -33,24 +39,5 @@ def transform_to_jsonl() -> str:
     return str(jsonl_path)
 
 
-def start_fine_tune() -> None:
-    abs_path = Path(__file__).parent
-    jsonl_path = (abs_path / "data/examples.jsonl").resolve()
-    upload = openai.File.create(file=open(jsonl_path, "rb"), purpose="fine-tune")
-    resp = openai.FineTune.create(training_file=upload.id, model="davinci")
-    print("To follow the model and see when it's done, please run the following:")
-    print(f"openai api fine_tunes.follow -i {resp['id']}")
-
-
 if __name__ == "__main__":
-    # STEP 1, run this:
     transform_to_jsonl()
-
-    # STEP 2
-    # add completion data to the jsonl
-
-    # STEP 3
-    # start_fine_tune()
-
-    # STEP 4
-    # mark all funcs as fine tuned?
