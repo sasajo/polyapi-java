554c8b59b776681ea2f396dfa742279c979b593d
Dan Fellin <dan@highwaterlabs.com>
2023-03-21 11:29:58 -0700

    next

--- science/completion.py
+++ science/completion.py
@@ -119,7 +119,7 @@ def get_completion_prompt_messages(
     functions: str, webhooks: str, question: str
 ) -> List[Dict]:
     return [
-        {"role": "system", "content": "Include argument types. Be concise. Only include functions and event handlers explicitly defined by the Poly API library."},
+        {"role": "system", "content": "Include argument types. Be concise."},
         {"role": "assistant", "content": functions},
         {"role": "assistant", "content": webhooks},
         {"role": "user", "content": question},
