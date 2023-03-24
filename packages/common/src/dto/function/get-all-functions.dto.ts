b71d7f2f8a6eb1f7818a0aabf42741c901a13771
Vladimir Hrusovsky <vladimir.hrusovsky@hotovo.com>
2023-03-24 09:11:25 +0100

    Fixed issue with contexts/names/ids generation from library

--- packages/common/src/dto/function/get-all-functions.dto.ts
+++ packages/common/src/dto/function/get-all-functions.dto.ts
@@ -6,18 +6,18 @@ export class GetAllFunctionsDto {
   @IsArray()
   @IsString({ each: true })
   @Type(() => String)
-  @Transform(({ value }) => value.split(','))
+  @Transform(({ value }) => Array.isArray(value) ? value : value.split(','))
   contexts?: string[];
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @Type(() => String)
-  @Transform(({ value }) => value.split(','))
+  @Transform(({ value }) => Array.isArray(value) ? value : value.split(','))
   names?: string[];
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @Type(() => String)
-  @Transform(({ value }) => value.split(','))
+  @Transform(({ value }) => Array.isArray(value) ? value : value.split(','))
   ids?: string[];
 }
