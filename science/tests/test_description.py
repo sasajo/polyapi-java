d160c0637d414bb6ef4de8157ab41e2a0b8a8875
Dan Fellin <dan@highwaterlabs.com>
2023-04-25 09:52:42 -0700

    killing it

--- science/tests/test_description.py
+++ science/tests/test_description.py
@@ -1,9 +1,11 @@
 import json
 import unittest
 from unittest.mock import patch
-from app.description import _get_context_and_names, _parse_openai_response, get_function_description, \
-  get_webhook_description
-from load_fixtures import load_functions, test_user_get_or_create
+from app.description import (
+    _parse_openai_response,
+    get_function_description,
+    get_webhook_description,
+)
 from .testing import DbTestCase
 
 
@@ -11,7 +13,9 @@ class T(DbTestCase):
     @patch("app.description.openai.ChatCompletion.create")
     def test_get_function_description(self, mock_chat):
         mock_content = {"description": "foobar", "name": "foo", "context": "ba-r"}
-        mock_chat.return_value = {'choices': [{'message': {'content': json.dumps(mock_content)}}]}
+        mock_chat.return_value = {
+            "choices": [{"message": {"content": json.dumps(mock_content)}}]
+        }
         output = get_function_description(
             {
                 "url": "https://example.com",
@@ -20,25 +24,27 @@ class T(DbTestCase):
             }
         )
         self.assertEqual(mock_chat.call_count, 1)
-        self.assertEqual(output['name'], 'foo')
-        self.assertEqual(output['context'], 'bar')
-        self.assertEqual(output['description'], 'foobar')
+        self.assertEqual(output["name"], "foo")
+        self.assertEqual(output["context"], "bar")
+        self.assertEqual(output["description"], "foobar")
 
     @patch("app.description.openai.ChatCompletion.create")
     def test_get_webhook_description(self, mock_chat):
-      mock_content = {"description": "foobar", "name": "foo", "context": "ba-r"}
-      mock_chat.return_value = {'choices': [{'message': {'content': json.dumps(mock_content)}}]}
-      output = get_webhook_description(
-        {
-          "url": "https://example.com",
-          "method": "POST",
-          "short_description": "Create a thing",
+        mock_content = {"description": "foobar", "name": "foo", "context": "ba-r"}
+        mock_chat.return_value = {
+            "choices": [{"message": {"content": json.dumps(mock_content)}}]
         }
-      )
-      self.assertEqual(mock_chat.call_count, 1)
-      self.assertEqual(output['name'], 'foo')
-      self.assertEqual(output['context'], 'bar')
-      self.assertEqual(output['description'], 'foobar')
+        output = get_webhook_description(
+            {
+                "url": "https://example.com",
+                "method": "POST",
+                "short_description": "Create a thing",
+            }
+        )
+        self.assertEqual(mock_chat.call_count, 1)
+        self.assertEqual(output["name"], "foo")
+        self.assertEqual(output["context"], "bar")
+        self.assertEqual(output["description"], "foobar")
 
     @unittest.skip("Old approach not asking OpenAI to return JSON")
     def test_parse_function_description(self):
@@ -49,13 +55,16 @@ class T(DbTestCase):
         Description:
         This API call retrieves a list of members subscribed..."""
         parsed = _parse_openai_response(completion)
-        self.assertEqual(parsed['context'], 'comms.mailchimp')
-        self.assertEqual(parsed['name'], 'mailchimp.getMembers')
-        self.assertEqual(parsed['description'], 'This API call retrieves a list of members subscribed...')
+        self.assertEqual(parsed["context"], "comms.mailchimp")
+        self.assertEqual(parsed["name"], "mailchimp.getMembers")
+        self.assertEqual(
+            parsed["description"],
+            "This API call retrieves a list of members subscribed...",
+        )
 
-    def test_get_contexts_and_names(self):
-        user = test_user_get_or_create
-        load_functions(user)
-        contexts = _get_context_and_names()
-        self.assertIn("hospitality.opera.getHotelDetails", contexts)
-        self.assertIn("travel.unitedAirlines.getFlights", contexts)
+    # def test_get_contexts_and_names(self):
+    #     user = test_user_get_or_create
+    #     load_functions(user)
+    #     contexts = _get_context_and_names()
+    #     self.assertIn("hospitality.opera.getHotelDetails", contexts)
+    #     self.assertIn("travel.unitedAirlines.getFlights", contexts)
