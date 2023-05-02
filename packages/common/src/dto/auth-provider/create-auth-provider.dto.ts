06f8b185c49869af99e0413fc6e24ac6f91d8367
Vladimir Hrusovsky <vladimir.hrusovsky@hotovo.com>
2023-05-02 10:10:47 +0200

    Added Url checks for auth providers

--- packages/common/src/dto/auth-provider/create-auth-provider.dto.ts
+++ packages/common/src/dto/auth-provider/create-auth-provider.dto.ts
@@ -1,14 +1,28 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsUrl } from 'class-validator';
 
 export class CreateAuthProviderDto {
   @IsNotEmpty()
   context: string;
   @IsNotEmpty()
+  @IsUrl({
+    require_protocol: true,
+  })
   authorizeUrl: string;
   @IsNotEmpty()
+  @IsUrl({
+    require_protocol: true,
+  })
   tokenUrl: string;
   audienceRequired?: boolean;
   refreshEnabled?: boolean;
+  @IsUrl({
+    require_protocol: true,
+  })
+  @IsOptional()
   revokeUrl?: string;
+  @IsUrl({
+    require_protocol: true,
+  })
+  @IsOptional()
   introspectUrl?: string;
 }
--- packages/common/src/dto/auth-provider/update-auth-provider.dto.ts
+++ packages/common/src/dto/auth-provider/update-auth-provider.dto.ts
@@ -1,9 +1,27 @@
+import { IsOptional, IsUrl } from 'class-validator';
+
 export class UpdateAuthProviderDto {
   context?: string;
+  @IsUrl({
+    require_protocol: true,
+  })
+  @IsOptional()
   authorizeUrl?: string;
+  @IsUrl({
+    require_protocol: true,
+  })
+  @IsOptional()
   tokenUrl?: string;
   audienceRequired?: boolean;
   refreshEnabled?: boolean;
+  @IsUrl({
+    require_protocol: true,
+  })
+  @IsOptional()
   revokeUrl?: string | null;
+  @IsUrl({
+    require_protocol: true,
+  })
+  @IsOptional()
   introspectUrl?: string | null;
 }
