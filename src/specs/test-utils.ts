d70c5dddaacf67462b9f5b255cf5ac7b32fc36e6
Vladimir Hrusovsky <vladimir.hrusovsky@hotovo.com>
2023-06-07 12:38:20 +0200

    Fixed test-utils

--- src/specs/test-utils.ts
+++ src/specs/test-utils.ts
@@ -1,8 +1,9 @@
-import { ExecutionContext } from "@nestjs/common";
+import { ExecutionContext } from '@nestjs/common';
 import { PolyKeyGuard } from 'auth/poly-key-auth-guard.service';
 
-import { Role } from "../../packages/common/src/user";
-import { AuthData } from "common/types";
+import { Role } from '@poly/common';
+import { AuthData } from 'common/types';
+import { Permission } from '@poly/common';
 
 const currentDate = new Date();
 
@@ -10,7 +11,7 @@ const currentDate = new Date();
  * remap methods from a type in a new one.
  */
 export type ExtractMethods<Type> = {
-    [Key in keyof Type as (Type[Key] extends (...args: any[]) => any ? Key : never)]: Type[Key];
+  [Key in keyof Type as (Type[Key] extends (...args: any[]) => any ? Key : never)]: Type[Key];
 }
 
 /**
@@ -18,49 +19,61 @@ export type ExtractMethods<Type> = {
  */
 export type TypedMock<T> = Partial<ExtractMethods<T>>;
 
+const tenantId = '2a918df0-dffd-4773-b2b4-2b5cd3182c5f';
 /**
  * Default mocked data used by `getMockedPolyKeyGuard`.
  */
 export const mockedAuthData: AuthData = {
-    user: {
-        id: '3ba47ec4-8871-4fea-a221-c2e322acc3a6',
-        name: 'foo',
-        role: Role.Admin,
-        vip: false,
-        teamId: '2a918df0-dffd-4773-b2b4-2b5cd3182c5f',
-        createdAt: currentDate
-    },
-    environment: {
-        appKey: '',
-        createdAt: currentDate,
-        id: '',
-        name: '',
-        subdomain: '',
-        tenantId: ''
-    },
-    tenant: {
-        createdAt: currentDate,
-        id: '',
-        name: ''
-    }
-}
+  user: {
+    id: '3ba47ec4-8871-4fea-a221-c2e322acc3a6',
+    name: 'foo',
+    role: Role.Admin,
+    vip: false,
+    tenantId,
+    createdAt: currentDate,
+  },
+  environment: {
+    createdAt: currentDate,
+    id: '',
+    name: '',
+    subdomain: '',
+    tenantId: '',
+  },
+  tenant: {
+    createdAt: currentDate,
+    id: '',
+    name: '',
+  },
+  key: 'b4d1fd5c-8dc2-45e5-9bf3-047a2bcce2ca',
+  application: {
+    createdAt: currentDate,
+    id: 'e3f8af34-eac2-4f66-8da3-3acd38c65eca',
+    name: 'application',
+    tenantId,
+    description: '',
+  },
+  permissions: {
+    [Permission.CustomDev]: true,
+    [Permission.Teach]: true,
+  },
+};
 
 /**
  * Get mocked `PolyApiKeyGuard`.
  */
-export function getMockedPolyKeyGuard(user: AuthData = mockedAuthData): TypedMock<PolyKeyGuard>{
+export function getMockedPolyKeyGuard(user: AuthData = mockedAuthData): TypedMock<PolyKeyGuard> {
 
-    return {
-        async canActivate(context: ExecutionContext): Promise<any> {
-            context.switchToHttp().getRequest().user = user;
-            return true;
-        }
-    }
+  return {
+    async canActivate(context: ExecutionContext): Promise<any> {
+      context.switchToHttp().getRequest().user = user;
+      return true;
+    },
+  };
 }
 
 /**
  * Get typed mock using `jest.fn`.
  */
 export function getFnMock<T extends (...args: any) => any>() {
-    return jest.fn<ReturnType<T>, Parameters<T>>();
+  return jest.fn<ReturnType<T>, Parameters<T>>();
 }
