3566216a078568be29e6a66e58398c6f9d1eb618
Dan Fellin <dan@highwaterlabs.com>
2023-06-08 12:52:29 -0700

    docker tweaks

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-DB="${PRISMA_HOME_DIR}/db/poly.db" 
-if [ -f $DB ]; then
-    echo "Using existing db at ${DB}"   
-else
-    echo "Recreating ${DB}"
-    yarn prisma migrate deploy
-fi
+yarn prisma migrate deploy
 yarn run build
 nohup yarn run start:prod &
 cd science
