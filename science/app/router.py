fe30771721dd51942f88c401b2a18e456c3417ca
Dan Fellin <dan@highwaterlabs.com>
2023-06-27 11:54:28 -0700

    lets just omit general as an option

--- science/app/router.py
+++ science/app/router.py
@@ -9,7 +9,7 @@ Please categorize the user's question. Here are the categories:
     "function": "The user is looking for a function or details about a function to address their need",
     "conversation": "The question seems incomplete suggesting the user is referencing information from the preceding conversation",
     "documentation": "The user is looking to understand how to do something specific with PolyAPI or has a general question about PolyAPI",
-    "general": "The user is asking a general programming or informational question"
+    %s
 }
 ```
 
@@ -26,7 +26,12 @@ Here is the question:
 
 
 def route_question(question: str) -> str:
-    prompt = ROUTER_PROMPT % question.replace("Poly", "PolyAPI").replace("poly", "PolyAPI")
+    if "poly" in question.lower():
+        general = '"general": "The user is asking a general programming or informational question"'
+    else:
+        general = ""
+
+    prompt = ROUTER_PROMPT % (general, question)
     choice = simple_chatgpt_question(prompt)
     content = json.loads(choice['message']['content'])
     return content['category']
