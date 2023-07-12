1479763612eb418863d2be6d2fa2cd2dee81f24a
adrianlocurciohotovo <115090431+adrianlocurciohotovo@users.noreply.github.com>
2023-07-12 02:41:59 -0300

    Hotfix for Access token bug (#658)
    Co-authored-by: Adrián Locurcio <adrianlocurcio@MacBook-Pro-de-Admin.local>

--- src/auth/poly-auth-guard.service.ts
+++ src/auth/poly-auth-guard.service.ts
@@ -2,6 +2,7 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Role } from '@poly/common';
 import { AuthData } from 'common/types';
+import { Request } from 'express';
 
 @Injectable()
 export class PolyAuthGuard extends AuthGuard('bearer') {
@@ -9,13 +10,43 @@ export class PolyAuthGuard extends AuthGuard('bearer') {
     super();
   }
 
+  /**
+   * If user sends `access_token` through body request payload or query params, `canActivate` throws a 401 Unauthorized error.
+   * This is because passport strategy also checks `access_token`, see docs: https://github.com/jaredhanson/passport-http-bearer#making-authenticated-requests
+   * Since we are using `access_token` through `Authorization` header, we have to clean key `access_token` from body and query until we execute `canActivate` method.
+   */
+  private cleanAccessTokenFromBodyAndQuery(request: Request): { bodyCopy: typeof request.body, queryCopy: typeof request.query} {
+    const queryCopy = Object.assign({}, request.query);
+    const bodyCopy = Object.assign({}, request.body);
+
+    delete request.query['access_token'];
+
+    if (request.headers['content-type']?.match(/application\/json/)) {
+      delete request.body['access_token'];
+    }
+
+    return {
+      bodyCopy,
+      queryCopy,
+    };
+  }
+
   async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const {
+      bodyCopy,
+      queryCopy,
+    } = this.cleanAccessTokenFromBodyAndQuery(request);
+
     const result = (await super.canActivate(context)) as boolean;
+
+    (request as Request).body = bodyCopy;
+    (request as Request).query = queryCopy;
+
     if (!result) {
       return false;
     }
 
-    const request = context.switchToHttp().getRequest();
     if (!request.user) {
       return false;
     }
