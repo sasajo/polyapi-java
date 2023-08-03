b9feac9a473a2892c5068ca1f71f56922426c57c
Bboydozzy96 <federico.marchini@hotovo.com>
2023-08-03 10:58:36 -0300

    let's try this

--- src/function/faas/knative/knative-faas.service.ts
+++ src/function/faas/knative/knative-faas.service.ts
@@ -278,7 +278,7 @@ export class KNativeFaasService implements FaasService {
                 ],
                 command: ['/bin/sh', '-c'],
                 args: [
-                  `mv /workspace/function/${workingDir}/function/index.js ../../../../../`,
+                  `/cnb/lifecycle/launcher mv /${workingDir}/function/index.js ../../../../../`,
                   '/cnb/lifecycle/launcher "npx poly generate && npm start"',
                 ],
                 workingDir: `/workspace/function/${workingDir}/function`,
