88d1ffa942f1bf0f69b31d36032b752ae8965496
adrianlocurciohotovo <115090431+adrianlocurciohotovo@users.noreply.github.com>
2023-09-05 12:00:59 -0300

    Sign up email templates (#919)
    * Templates usage.
    
    * Removed duplicated log.
    
    ---------
    
    Co-authored-by: Adrián Locurcio <adrianlocurcio@MacBook-Pro-de-Admin.local>

--- src/email/provider/mailchimp/index.ts
+++ src/email/provider/mailchimp/index.ts
@@ -11,6 +11,7 @@ export default class Mailchimp implements EmailServiceProvider {
   }
 
   async send(fromEmail: string, subject: string, text: string, to: string): Promise<any> {
+    this.logger.debug(`Sending email with values: "fromEmail": "${fromEmail}", "subject": "${subject}", "text": "${text}", "to": "${to}"`);
     const response = await this.mailchimpTx.messages.send({
       message: {
         from_email: fromEmail,
@@ -22,6 +23,31 @@ export default class Mailchimp implements EmailServiceProvider {
             type: 'to',
           },
         ],
+      },
+    });
+
+    this.logger.debug('Email provider response', response);
+
+    return response;
+  }
+
+  async sendEmailTemplate<T extends Record<string, any>>(fromEmail: string, subject: string, to: string, templateName: string, variables: T): Promise<any> {
+    this.logger.debug(`Sending template email with values "fromEmail": "${fromEmail}", "subject": "${subject}",  "to": "${to}", "templateName": "${templateName}", "variables": ${JSON.stringify(variables)}`);
+    const response = await this.mailchimpTx.messages.sendTemplate({
+      template_name: templateName,
+      template_content: [],
+      message: {
+        from_email: fromEmail,
+        subject,
+        to: [
+          {
+            email: to,
+            type: 'to',
+          },
+        ],
+        global_merge_vars: Object.entries(variables).reduce((acum, [key, value]) => {
+          return [...acum, { name: key, content: value }];
+        }, []),
       },
     });
 
