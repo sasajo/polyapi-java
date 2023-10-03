c280c014bf4fb2fa2c23ef6ee6140a9ac5ee2712
Vladimir Hrusovsky <vladimir.hrusovsky@hotovo.com>
2023-10-03 13:35:26 +0200

    Webhook training pre-request script updated

--- postman/scripts/webhook-training-pre-request.js
+++ postman/scripts/webhook-training-pre-request.js
@@ -2,16 +2,16 @@ const postmanCollection = require('postman-collection');
 
 const templateBodyParts = pm.request.body.raw.split('{');
 
-templateBodyParts[1] = `"templateBody": "${btoa(pm.request.body.raw)}", ${templateBodyParts[1]}`
+templateBodyParts[1] = `"templateBody": ${JSON.stringify(pm.request.body.raw)}, ${templateBodyParts[1]}`
 
 pm.request.update({
-    body: new postmanCollection.RequestBody({
-        mode: 'raw',
-        raw: templateBodyParts.join('{'),
-        options: {
-            raw: {
-                language: 'json'
-            }
-        }
-    })
-})
+  body: new postmanCollection.RequestBody({
+    mode: 'raw',
+    raw: templateBodyParts.join('{'),
+    options: {
+      raw: {
+        language: 'json'
+      }
+    }
+  })
+});
