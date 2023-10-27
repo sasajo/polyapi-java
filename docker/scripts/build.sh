bcba20d75b2186fc098dde5e5875d51405d7f67c
Dan Fellin <dan@highwaterlabs.com>
2023-10-27 09:35:25 -0700

    first attempt at obfuscating the code

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -1,6 +1,15 @@
-
 #!/bin/bash
 source .env
+
+pip3 install pyarmor==8.4.1
+
+cd science
+pyarmor gen -O app_o -r app
+rm -rf app
+mv app_o/* ./
+rmdir app_o
+cd ..
+
 docker build --progress=plain \
   -t ghcr.io/polyapi/polyapi:latest \
   -f docker/Dockerfile \
