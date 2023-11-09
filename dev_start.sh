31037f25d9de0968be47151d9c312d4fa5e320e2
Dan Fellin <dan@highwaterlabs.com>
2023-11-09 06:03:38 -0800

    lets and everythign

--- dev_start.sh
+++ dev_start.sh
@@ -2,5 +2,4 @@
 docker start redis 2>/dev/null || docker run --name redis -p 6379:6379 -d redis
 docker start postgres 2>/dev/null || docker run -e POSTGRES_USER=polyapi -e POSTGRES_PASSWORD=secret --name postgres -p 5432:5432 -d postgres
 vault server -dev -dev-root-token-id root &
-SKIP_KNATIVE=1 yarn run start:dev &
-cd science && flask --app app run --debug
+SKIP_KNATIVE=1 yarn run start:dev && cd science && flask --app app run --debug
