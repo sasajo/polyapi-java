fd14405e741ca7bc72aa0affa3869c22af9de1a6
Dan Fellin <dan@highwaterlabs.com>
2023-12-12 09:01:07 -0800

    maybe this

--- src/function/faas/knative/knative-faas.service.ts
+++ src/function/faas/knative/knative-faas.service.ts
@@ -576,7 +576,7 @@ export class KNativeFaasService implements FaasService {
     // const startUpCommand = `if [ -f "/workspace/function/func.py" ];
     //    cp -f /workspace/function/func.py /workspace/ && pip3 install git+https://github.com/polyapi/polyapi-python.git && /workspace/app.sh
     //   else python3 -m polyapi generate; fi`;
-    const startUpCommand = '/cnb/lifecycle/launcher "cp /workspace/function/* /workspace/ && /workspace/app.sh"'; // 'cp -f /workspace/function/* /workspace/ && pip install -r requirements.txt && ls && /workspace/app.sh';
+    const startUpCommand = '/cnb/lifecycle/launcher "cd /workspace/function/ && ./app.sh"'; // 'cp -f /workspace/function/* /workspace/ && pip install -r requirements.txt && ls && /workspace/app.sh';
 
     const args = [startUpCommand];
 
