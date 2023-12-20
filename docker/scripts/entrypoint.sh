39c1ba771eb684402f95c2cc859f76da836c14e5
Bboydozzy96 <federico.marchini@hotovo.com>
2023-12-20 15:57:36 -0300

    edited dockerfile

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 yarn prisma migrate deploy
-nohup yarn run start:prod &
 cd science
 nohup uwsgi --ini ./uwsgi.ini
+
+# Function to handle the SIGTERM signal
+function handle_sigterm() {
+    echo "Received SIGTERM, shutting down gracefully"
+    kill -TERM "$child_pid"
+}
+
+# Set up signal trapping
+trap handle_sigterm SIGTERM
+
+# Run the application server and store its process ID
+nohup yarn run start:prod &
+child_pid=$!
+
+echo $child_pid
+
+# Wait for the child process to exit
+wait "$child_pid"
