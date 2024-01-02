8a709e7ab07253fe5635033c66849d11a0f690a0
Dan Fellin <dan@highwaterlabs.com>
2024-01-02 13:22:52 -0800

    maybe put warning first?

--- src/function/faas/loki-logs/loki-logs.service.ts
+++ src/function/faas/loki-logs/loki-logs.service.ts
@@ -133,7 +133,7 @@ export class LokiLogsService implements FaasLogsService {
   }
 
   private getLogContentAndLevel(rawLogText: string): [logContent: string, logLevel: string] {
-    const polyLogPattern = /\[(ERROR|LOG|INFO|WARN|WARNING|DEBUG|CRITICAL)](.*?)\[\/\1]/gs;
+    const polyLogPattern = /\[(ERROR|LOG|INFO|WARNING|WARN|DEBUG|CRITICAL)](.*?)\[\/\1]/gs;
     const timestampStdPipePattern = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z (stdout|stderr) F /g;
 
     const [, level, logText] = polyLogPattern.exec(rawLogText) || [null, 'UNKNOWN', rawLogText];
