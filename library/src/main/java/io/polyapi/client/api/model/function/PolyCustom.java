a2748e1ba93da032ab14ed794811093df9dff3eb
Gastón Ortiz <gaston.ortiz@hotovo.com>
2024-05-14 17:29:27 -0300

    Added PolyCustom feature for server functions.

--- library/src/main/java/io/polyapi/client/api/model/function/PolyCustom.java
+++ library/src/main/java/io/polyapi/client/api/model/function/PolyCustom.java
@@ -1,13 +1,26 @@
 package io.polyapi.client.api.model.function;
 
 import io.polyapi.commons.api.model.PolyObject;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
- * Utility class that provides additional functionality to server functions. Available only during execution time.
+ * Utility class that provides additional functionality and information to server functions. Available only during execution time.
  */
 @Getter
-public class PolyCustom implements PolyObject {
-    private String environmentId;
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public final class PolyCustom implements PolyObject {
+    private String executionId;
+    private String executionApiKey;
+
+    @Setter
+    private Integer responseStatusCode;
+
+    @Setter
+    private String responseContentType;
 }
